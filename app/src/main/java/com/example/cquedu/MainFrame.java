d782771cc008cce4f7bd981dc22e887b5d0ae030
Violape <Dr_Otaku@yeah.net>
2019-03-12 13:35:28 +0800

    Log out button

--- app/src/main/java/com/example/cquedu/MainFrame.java
+++ app/src/main/java/com/example/cquedu/MainFrame.java
@@ -1,7 +1,11 @@
 package com.example.cquedu;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainFrame extends AppCompatActivity {
 
@@ -10,4 +14,33 @@ public class MainFrame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_frame);
     }
+
+    private DialogInterface.OnClickListener click1=new DialogInterface.OnClickListener()
+    {
+        public void onClick(DialogInterface arg0,int arg1)
+        {
+            //quit current log in
+            //return to the login page
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+    };
+
+    private DialogInterface.OnClickListener click2=new DialogInterface.OnClickListener()
+    {
+        @Override
+        public void onClick(DialogInterface arg0,int arg1)
+        {
+            arg0.cancel();
+        }
+    };
+
+    public void onlogout(View view) {
+        AlertDialog.Builder alertdialogbuilder=new AlertDialog.Builder(this);
+        alertdialogbuilder.setMessage("Are you sure to log out?");
+        alertdialogbuilder.setPositiveButton("Yes",click1);
+        alertdialogbuilder.setNegativeButton("No",click2);
+        AlertDialog alertdialog1=alertdialogbuilder.create();
+        alertdialog1.show();
+    }
+
 }
