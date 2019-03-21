9e49da35f42b0bebb736e2707c8adb359e461d74
Violape <Dr_Otaku@yeah.net>
2019-03-21 18:42:53 +0800

    Creation of class schedule page

---
+++ app/src/main/java/com/example/cquedu/ClassSchedule.java
@@ -0,0 +1,23 @@
+package com.example.cquedu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ClassSchedule extends AppCompatActivity {
+    private Intent intent;
+    private String myuser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_schedule);
+
+        intent = getIntent();
+        myuser = intent.getStringExtra("CQUID");
+        TextView signinfo = findViewById(R.id.i_tv_signinfo);
+        signinfo.setText("Current User: "+ myuser);
+
+    }
+}
--- app/src/main/java/com/example/cquedu/MainFrame.java
+++ app/src/main/java/com/example/cquedu/MainFrame.java
@@ -65,4 +65,13 @@ public class MainFrame extends AppCompatActivity {
         intent2.putExtra("CQUID", cquid);
         startActivity(intent2);
     }
+
+    public void onClickClassSchedule(View view){
+        Intent intent1 = getIntent();
+        String cquid = intent1.getStringExtra("CQUID");
+        Intent intent2 = new Intent();
+        intent2.setClass(MainFrame.this, ClassSchedule.class);
+        intent2.putExtra("CQUID", cquid);
+        startActivity(intent2);
+    }
 }
