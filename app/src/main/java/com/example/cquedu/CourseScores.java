e472b966af4ca036603abcc0f0e07193b568fd78
Violape <Dr_Otaku@yeah.net>
2019-03-22 20:13:20 +0800

    Basic Structure of Score Check Activity

--- app/src/main/java/com/example/cquedu/CourseScores.java
+++ app/src/main/java/com/example/cquedu/CourseScores.java
@@ -1,13 +1,50 @@
 package com.example.cquedu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CourseScores extends AppCompatActivity {
+    private Intent intent;
+    private String myuser;
+    MyApplication application;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_scores);
+
+        application = (MyApplication)this.getApplication();
+
+        intent = getIntent();
+        myuser = intent.getStringExtra("CQUID");
+        TextView signinfo = findViewById(R.id.p_tv_signinfo);
+        signinfo.setText("Current User: "+ myuser);
+
+        Button btn = findViewById(R.id.p_bt_return);
+        btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Button mybtn = findViewById(R.id.p_bt_return);
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        mybtn.setBackgroundResource(R.drawable.buttononpressed);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        mybtn.setBackgroundResource(R.drawable.buttonbg);
+                        onReturn();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void onReturn(){
+        finish();
     }
 }
