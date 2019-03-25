9f93b1fc40909e0c93dba1e56eb4db08703cb15f
Violape <Dr_Otaku@yeah.net>
2019-03-25 22:09:30 +0800

    Connection to the server

--- app/src/main/java/com/example/cquedu/ScoreComparison.java
+++ app/src/main/java/com/example/cquedu/ScoreComparison.java
@@ -6,16 +6,25 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import okhttp3.FormBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class ScoreComparison extends AppCompatActivity {
     Intent intent;
     private String myuser;
+    MyApplication application;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_comparison);
+
+        application = (MyApplication)this.getApplication();
 
         intent = getIntent();
         myuser = intent.getStringExtra("CQUID");
@@ -39,6 +48,43 @@ public class ScoreComparison extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    public void onCompare(View view){
+        Spinner sp = findViewById(R.id.c_sp_term);
+        int element = sp.getSelectedItemPosition();
+        String term = String.valueOf((element+1)*5+(element%2));
+        EditText et = findViewById(R.id.c_et_cquid);
+        String cquID = et.getText().toString();
+        double myScore = getScore(term, myuser);
+        double yrScore = getScore(term, cquID);
+    }
+
+    private double getScore(String term, String user){
+        RequestBody payload = new FormBody.Builder()
+                .add("chk","1")
+                .add("chkrad","1")
+                .add("sel_xnxq", term)
+                .add("sel_xs", user)
+                .add("txt_xs", user)
+                .build();
+        Request score = new Request.Builder()
+                .url("http://jxgl.cqu.edu.cn/xscj/f_xscjtzd_rpt.aspx")
+                .post(payload)
+                .addHeader("origin", "http://jxgl.cqu.edu.cn")
+                .addHeader("upgrade-insecure-requests", "1")
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .addHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36")
+                .addHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+                .build();
+        String infores = "";
+        try {
+            infores = new String(application.license.newCall(score).execute().body().bytes(),"gbk");
+        }
+        catch (Exception e){
+            return -1;
+        }
+        return 0;
     }
 
     public void onReturn(){
